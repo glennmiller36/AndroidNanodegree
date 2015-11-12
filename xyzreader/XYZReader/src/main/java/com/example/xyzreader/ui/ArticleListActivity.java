0791132b1ff4afa5bf9cdcc8ddbfcd08099b9d62
Glenn Miller <glennmiller36@gmail.com>
2015-11-11 19:42:15 -0600

    feat: Add FloatingActionButton to Activity List to refresh list

--- xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,15 +8,12 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
-import android.util.TypedValue;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -36,7 +33,6 @@ public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
     private Toolbar mToolbar;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -46,12 +42,15 @@ public class ArticleListActivity extends AppCompatActivity implements
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
-
-        //mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
-
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        FloatingActionButton fabRefresh = (FloatingActionButton) findViewById(R.id.fab);
+        fabRefresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                refresh();
+            }
+        });
+
         getLoaderManager().initLoader(0, null, this);
 
         if (savedInstanceState == null) {
@@ -83,14 +82,9 @@ public class ArticleListActivity extends AppCompatActivity implements
         public void onReceive(Context context, Intent intent) {
             if (UpdaterService.BROADCAST_ACTION_STATE_CHANGE.equals(intent.getAction())) {
                 mIsRefreshing = intent.getBooleanExtra(UpdaterService.EXTRA_REFRESHING, false);
-                updateRefreshingUI();
             }
         }
     };
-
-    private void updateRefreshingUI() {
-        /*mSwipeRefreshLayout.setRefreshing(mIsRefreshing)*/;
-    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
